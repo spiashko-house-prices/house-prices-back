fba7629ca44fac58f1cca33dca292e3921c4b579
Siarhei Piashko <siarhei.piashko@ihg.com>
2018-04-21 21:37:04 +0300

    update run file

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-docker run -it --rm --name house-prices-back -p 5000:5000 -v $PWD/instance_in_docker:/home/sources/instance \
+
+MONGODB_URI=$(heroku config:get MONGODB_URI) && docker run -it --rm --name house-prices-back -p 5000:5000 -e="MONGODB_URI=$MONGODB_URI" \
 house-prices-back
