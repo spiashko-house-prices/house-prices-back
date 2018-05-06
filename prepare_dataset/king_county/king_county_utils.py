3941f429e0900ee3934d4abae54b3e3611757fd2
Siarhei Piashko <siarhei.piashko@ihg.com>
2018-05-06 18:46:50 +0300

    add scripts to upload king_county dataset

---
+++ prepare_dataset/king_county/king_county_utils.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+
+def make_transform(frame):
+    """
+    Transform some numerical features to categorical.
+    :type frame: pd.DataFrame
+    :param frame:
+    """
+    # Some numerical features are actually really categories
+    frame.replace(
+        {
+            "waterfront": {0: "No", 1: "Yes"}
+        },
+        inplace=True
+    )
+
+
+def parse_date(frame):
+    """
+    Transform some numerical features to categorical.
+    :type frame: pd.DataFrame
+    :param frame:
+    """
+    frame["year_sold"] = frame["date"].apply(lambda row: int(row[:4]))
+    frame["month_sold"] = frame["date"].apply(lambda row: int(row[4:6]))
+    frame.replace(
+        {
+            "month_sold": {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun",
+                       7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"}
+        },
+        inplace=True
+    )
---
+++ prepare_dataset/king_county/upload_king_county.py
@@ -0,0 +1,27 @@
+import os
+
+import pandas as pd
+
+from prepare_dataset.king_county.king_county_utils import parse_date, make_transform
+from prepare_dataset.upload_dataset import upload
+
+
+# source https://www.kaggle.com/harlfoxem/housesalesprediction/
+
+df = pd.read_csv("kc_house_data.csv")
+
+parse_date(df)
+make_transform(df)
+
+df.drop(columns=['id', "date"], inplace=True)
+
+df.rename(index=str, columns={"price": "SalePrice"}, inplace=True)
+
+train_data = df[:11000]
+test_data = df[11000:]
+
+mongodb_url = os.environ['MONGODB_URI']
+
+upload(train_data, test_data, mongodb_url)
+
+print("done.")
